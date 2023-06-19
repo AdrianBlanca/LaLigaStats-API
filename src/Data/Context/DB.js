7f0bb8475c2e6fc31b8c78891d009fabd85b0708
Adrian Blanca <adrianblancaenriquez26@gmail.com>
2023-06-19 12:11:03 +0200

    database credentials changed

--- src/Data/Context/DB.js
+++ src/Data/Context/DB.js
@@ -1,8 +1,12 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize('LaLigaStats', 'postgres', 'root', {
-    host: 'localhost',
-    dialect: 'postgres'
+const sequelize = new Sequelize('laligastatsdb', 'laligastatsdb_user', 'eCpfV0Ef1poZD8l0hsi8yzvRcarZQLLi', {
+    host: 'dpg-ci824lunqql0ldf5t0q0-a.frankfurt-postgres.render.com',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: true,
+      native: true
+    }
   });
 
 
