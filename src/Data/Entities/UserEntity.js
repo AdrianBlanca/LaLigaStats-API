8e2620ad67943ba3f33d621dfec21faa6a593cd6
Adrian Blanca <adrianblancaenriquez26@gmail.com>
2023-06-19 12:22:47 +0200

    UserEntity changed

--- src/Data/Entities/UserEntity.js
+++ src/Data/Entities/UserEntity.js
@@ -1,4 +1,4 @@
-import {sequelize} from '../Context/db.js'
+import {sequelize} from '../Context/DB.js'
 import { DataTypes } from 'sequelize'
 
 export const UserEntity = sequelize.define('Usuario', {
